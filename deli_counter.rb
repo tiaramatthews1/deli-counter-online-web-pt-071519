6d52648074dc7af91934da8fb5f04c60a1dc0c16
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-22 13:41:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def deli_coutner(katz_deli)
+katz_deli = []
+other_deli = ["Logan", "Avi", "Spencer"]
+another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+end
+
+def line(katz_deli)
+  
+end
+
+def now_serving(katz_deli)
+  
+end
+
+def take_a_number(katz_deli)
